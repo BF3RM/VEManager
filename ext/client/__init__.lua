dc0e1d3235e32dc8b5e85f726b5c2cdec384d8e0
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-06-17 02:25:09 +0200

    Update __init__.lua

--- ext/client/__init__.lua
+++ ext/client/__init__.lua
@@ -3,7 +3,6 @@ night = require "presets/night"
 morning = require "presets/morning"
 noon = require "presets/noon"
 evening = require "presets/evening"
-easing = require "modules/easing"
 
 function VEManagerClient:__init()
     print('Initializing VEManagerClient')
@@ -68,6 +67,7 @@ end
 
 
 function VEManagerClient:RegisterModules()
+	easing = require "modules/easing"
 	require 'modules/time'
 	Time:__init()
 	--require '__shared/DebugGUI'
