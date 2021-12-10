f11277ad50dce23b42a9bee8d01d8b5230febd91
IllustrisJack <76592751+IllustrisJack@users.noreply.github.com>
2021-12-10 19:59:29 +0100

    feat(vem): disable expl ve patches

--- ext/Client/__init__.lua
+++ ext/Client/__init__.lua
@@ -228,10 +228,10 @@ end]]
 
 function VEManagerClient:OnPartitionLoad(p_Partition)
 
-	-- Explosion Patches
+	--[[ Explosion Patches
 	if VEM_CONFIG.PATCH_EXPLOSIONS_COLOR_CORRECTION then
 		g_Patches:ExplosionsVE(p_Partition)
-	end
+	end]]
 
 	-- Log Sky & Lighting Textures
 	g_Patches:LogComponents(p_Partition)
